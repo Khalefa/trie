b3f610a76569253e53fe4a418869c2c5c5051c37
Basio <m.elbasiouny@gmail.com>
2015-12-13 16:42:08 +0200

    Solve the exception with iterators are exhausted

--- src/eg/edu/alexu/ehr/ResultIterator.java
+++ src/eg/edu/alexu/ehr/ResultIterator.java
@@ -7,84 +7,71 @@ import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.Vector;
 
-public class ResultIterator implements Iterator<Integer>{
+public class ResultIterator implements Iterator<Integer> {
 
-
-public ResultIterator(List<RecordIterator> records_it) {
-	this.records_it=records_it;
-	Rs=new HashSet<>();
-	R_it=0;
-	R=records_it.get(R_it);
-	record=R.next();
-	Rs.add(record);
-	retrievals.add(R_it, Rs);
-	max_record_it=records_it.size();
-	
-	
-}
-int  R_it;
-int record;
-RecordIterator R;
-List<RecordIterator> records_it=null;
-Set<Integer> Rs=null;
-List<Set<Integer>> retrievals =new Vector<>() ;
-int max_record_it;
-@Override
-public boolean hasNext() {
-	// TODO Auto-generated method stub
-	for(RecordIterator t:records_it){
-		if(t.hasNext())
-			return true;
-	}
-		
-	return false;
-}
-void advance() {
-//
-//if(!R.hasNext()){
-//	R_it++;
-//	if(R_it==max_record_it){
-//		R_it=0;
-//	}
-	R_it=(R_it+1) % max_record_it;
-	
-		R=records_it.get(R_it);
-		record=R.next();
-		//retrievals.get(R_it).add(record);
-		Rs.add(record);
-		retrievals.add(R_it, Rs);
-	
-//}
-	
-
-	
-}
-public Integer intersect(){
-	Integer ret=-1;
-	while(ret==-1){
-		int i=0;
-		for(Set s:retrievals){
-			if (s.contains(record))
-				i++;
+	public ResultIterator(List<RecordIterator> records_it) {
+		this.records_it = records_it;
+		Rs = new HashSet<>();
+		R_it = 0;
+		R = records_it.get(R_it);
+		max_record_it = records_it.size();
+		if (R.hasNext()) {
+			record = R.next();
+			Rs.add(record);
+			retrievals.add(R_it, Rs);
 		}
-		if (i==retrievals.size())
-			ret=record;	
-		advance();
 	}
-	return ret;
-}
 
-@Override
-public Integer next() {
-	// TODO Auto-generated method stub
-	if (!hasNext())
-		throw new NoSuchElementException();
-	
-	
-	
-	return intersect();
-}
+	int R_it;
+	int record;
+	RecordIterator R;
+	List<RecordIterator> records_it = null;
+	Set<Integer> Rs = null;
+	List<Set<Integer>> retrievals = new Vector<>();
+	int max_record_it;
 
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		for (RecordIterator t : records_it) {
+			if (t.hasNext())
+				return true;
+		}
+		return false;
+	}
 
+	void advance() {
+		R_it = (R_it + 1) % max_record_it;
+		R = records_it.get(R_it);
+		if (R.hasNext()) {
+			record = R.next();
+			Rs.add(record);
+			retrievals.add(R_it, Rs);
+		}
+	}
+
+	public Integer intersect() {
+		Integer ret = -1;
+		while (ret == -1) {
+			int i = 0;
+			for (Set s : retrievals) {
+				if (s.contains(record))
+					i++;
+			}
+			if (i == retrievals.size())
+				ret = record;
+			advance();
+		}
+		return ret;
+	}
+
+	@Override
+	public Integer next() {
+		// TODO Auto-generated method stub
+		if (!hasNext())
+			throw new NoSuchElementException();
+
+		return intersect();
+	}
 
 }
